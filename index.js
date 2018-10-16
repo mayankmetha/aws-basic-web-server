1b5ad9b6cd15f0e8f669e7f7df7aa663279ef80a
mayankmetha <mayankmethad@gmail.com>
2018-10-16 11:09:01 +0530

    restructure content

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 var express = require("express");
 var path = require("path");
 var fs = require("fs");
-var cors = require('cors')
 
 var db = JSON.parse(fs.readFileSync(path.join(__dirname + "/payload/db")));
 
 var app = express();
-//app.use(cors);
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
@@ -16,18 +14,6 @@ app.use(function(req, res, next) {
 
 app.get("/books", (req, res) => {
     res.send(db)
-});
-
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname + "/payload/home.html"))
-});
-
-app.get("/jquery", (req, res) => {
-    res.sendFile(path.join(__dirname + "/payload/jquery.js"))
-});
-
-app.get("/script", (req, res) => {
-    res.sendFile(path.join(__dirname + "/payload/script.js"))
 });
 
 app.listen(80);
